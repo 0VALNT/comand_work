12f16c1ebdb5b61ba45287ca6deec44ba68e3ec4
StudentBI24 <vadimyxll@gmaol.com>
2024-12-27 15:22:05 +0300

    add file

---
+++ customAuth/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import CustomUser
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Обязательное поле. Введите действующий email.')
+    phone_number = forms.CharField(max_length=15, required=False, help_text='Необязательное поле.')
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'phone_number', 'password1', 'password2')
