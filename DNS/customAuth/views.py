7fc14cf87ddb91b9e0ba10b2dd4a99d389061c8c
StudentBI20 <StudentBI20@SYN13ASS-NB-504.alabuga.local>
2024-12-27 14:43:39 +0300

    Добавлен logout и профиль

--- DNS/customAuth/views.py
+++ DNS/customAuth/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.views import View
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 from django.views.generic import CreateView
 
@@ -16,3 +18,13 @@ def signup_view(request):
     else:
         form = SignUpForm()
     return render(request, 'reglog_form.html', {'form': form})
+
+
+@login_required(login_url='login')
+def profile(request):
+    return render(request, 'profile.html')
+
+def log_out(request):
+    response = redirect('login_user')
+    response.delete_cookie('sessionid')
+    return response
